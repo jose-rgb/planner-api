ba1a598211494403a7ff32afbbadf35ab531c241
José Ricardo <josericardosaraiva7@gmail.com>
2024-07-09 15:17:19 -0300

    fix: config cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from "fastify";
+import cors from '@fastify/cors';
 import { createTrip } from "./routes/create-trip";
 import { confirmTrip } from "./routes/confirm-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify();
+
+app.register(cors, {
+    origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
