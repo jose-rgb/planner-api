d4327c41bacca1dc0dd2e833b85a02d0e8ef141f
José Ricardo <josericardosaraiva7@gmail.com>
2024-07-09 15:12:28 -0300

    feat: structure create trip email

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import { createTrip } from "./routes/create-trip";
+import { confirmTrip } from "./routes/confirm-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify();
@@ -8,6 +9,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
+app.register(confirmTrip)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running..')
